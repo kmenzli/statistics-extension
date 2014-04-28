8d122af48e6357124d41033c01d545d9d8b39eb6
kmenzli <kmenzli@exoplatform.com>
2014-04-28 16:31:25 +0100

    PRESALES-288 : Use the PropertyConfigurator service to parametrize database

--- exo-addons-statistics-api/src/main/java/org/exoplatform/addons/statistics/api/Constants.java
+++ exo-addons-statistics-api/src/main/java/org/exoplatform/addons/statistics/api/Constants.java
@@ -5,13 +5,13 @@ package org.exoplatform.addons.statistics.api;
  */
 public interface Constants {
 
-    public static final String PROPERTY_DB_NAME = "dbName";
-    public static final String PROPERTY_DB_AUTHENTICATION = "dbAuthentication";
-    public static final String PROPERTY_DB_USER = "dbUser";
-    public static final String PROPERTY_DB_PASSWORD = "dbPassword";
+    public static final String PROPERTY_DB_NAME = "statistics.database.server.name";
+    public static final String PROPERTY_DB_AUTHENTICATION = "statistics.database.server.authentication";
+    public static final String PROPERTY_DB_USER = "statistics.database.server.user";
+    public static final String PROPERTY_DB_PASSWORD = "statistics.database.server.password";
     public static final String PROPERTY_SERVER_TYPE_EMBED = "embed";
-    public static final String PROPERTY_SERVER_TYPE = "dbServerType";
-    public static final String PROPERTY_SERVER_HOST = "dbServerHost";
-    public static final String PROPERTY_SERVER_PORT = "dbServerPort";
+    public static final String PROPERTY_SERVER_TYPE = "statistics.database.server.type";
+    public static final String PROPERTY_SERVER_HOST = "statistics.database.server.host";
+    public static final String PROPERTY_SERVER_PORT = "statistics.database.server.port";
 
 }
